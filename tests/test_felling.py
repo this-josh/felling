0a2b6379ddc4b81d6868cd709b1ed16a735eeca3
Josh <joshuaarkirk@hotmail.co.uk>
2021-03-02 10:31:37 +0000

    Added sanity check tests for all log_path types

--- tests/test_felling.py
+++ tests/test_felling.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import shutil
 
 
@@ -8,6 +9,28 @@ def test_configure_default_path():
 
     log_path = "./tests/logs"
     configure()
+
+    assert len(os.listdir(log_path)) == 1
+    shutil.rmtree(log_path)
+
+
+def test_configure_str_path():
+    """Quick sanity check test"""
+    from felling import configure
+
+    log_path = "./tests/logs"
+    configure(log_path)
+
+    assert len(os.listdir(log_path)) == 1
+    shutil.rmtree(log_path)
+
+
+def test_configure_pathlib_path():
+    """Quick sanity check test"""
+    from felling import configure
+
+    log_path = Path("./tests/logs")
+    configure(log_path)
 
     assert len(os.listdir(log_path)) == 1
     shutil.rmtree(log_path)
