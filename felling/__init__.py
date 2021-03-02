1b139fa06582a61fc5860327aa3fbf6a8f2238c0
Josh <joshuaarkirk@hotmail.co.uk>
2021-03-02 20:22:29 +0000

    update version num

--- felling/__init__.py
+++ felling/__init__.py
@@ -4,7 +4,7 @@ A package for consistent logging
 from felling.felling import configure
 
 __name__ = "felling"
-xyversion = "0.0.1"
+xyversion = "0.0.2"
 release = ""
 # combined version
 __version__ = f"{xyversion}{release}"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.0.1'
+version = "0.0.2"
 project_urls = {
     "Source": "https://github.com/this-josh/felling",
     "Tracker": "https://github.com/this-josh/felling/issues",
