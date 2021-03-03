a38bfd09e754feb43755d6d73b02a15b3c1bc123
Josh <joshuaarkirk@hotmail.co.uk>
2021-03-03 16:00:50 +0000

    initialise versioneer, need to verify it works still

--- felling/__init__.py
+++ felling/__init__.py
@@ -8,3 +8,7 @@ xyversion = "0.0.2"
 release = ""
 # combined version
 __version__ = f"{xyversion}{release}"
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
