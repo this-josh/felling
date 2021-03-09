11e0486374383601288da369c65549b0e457e78b
Josh <joshuaarkirk@hotmail.co.uk>
2021-03-09 20:15:52 +0000

    get version from ._version

--- felling/__init__.py
+++ felling/__init__.py
@@ -2,6 +2,7 @@
 A package for consistent logging
 """
 from felling.src.configure_felling import configure
+from felling._version import version
 
 __name__ = "felling"
-# combined version
+__version__ = version
