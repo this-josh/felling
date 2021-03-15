85e9ce20b5f9bc7b5b60d1178cd75b62e3d30c25
Josh <joshuaarkirk@hotmail.co.uk>
2021-03-15 12:01:56 +0000

    remove print statements, and to module not found error message

--- felling/__init__.py
+++ felling/__init__.py
@@ -6,19 +6,18 @@ from felling.src.configure_felling import configure
 __name__ = "felling"
 
 
-print("In felling __init__")
-
 # Felling supports multiple python versions which required different methods of getting the version
 try:
     from importlib.metadata import version as version_getter
     from importlib.metadata import PackageNotFoundError as NoPackageError
-    print("importlib")
 except ModuleNotFoundError:
     from pkg_resources import get_distribution as version_getter
     from pkg_resources import DistributionNotFound as NoPackageError
-    print("pkg_resources")
-except:
-    raise ModuleNotFoundError("Neither importlib nor pkg_resources are installed")
+except Exception:
+    # This should never happen
+    raise ModuleNotFoundError(
+        "Neither importlib nor pkg_resources are installed, cannot log felling version"
+    )
 
 try:
     __version__ = version_getter("felling")
