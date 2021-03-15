ce0af3a95ed65e870f9b3f634ce9a5da361c0fc0
Josh <joshuaarkirk@hotmail.co.uk>
2021-03-15 10:27:40 +0000

    Change how we get python version

--- felling/__init__.py
+++ felling/__init__.py
@@ -2,7 +2,19 @@
 A package for consistent logging
 """
 from felling.src.configure_felling import configure
-from felling.version import version
 
 __name__ = "felling"
-__version__ = version
+
+
+# Felling supports multiple python versions which required different methods of getting the version
+try:
+    from importlib.metadata import version as version_getter
+    from importlib.metadata import PackageNotFoundError as NoPackageError
+except ModuleNotFoundError:
+    from pkg_resources import get_distribution as version_getter
+    from pkg_resources import DistributionNotFound as NoPackageError
+
+try:
+    __version__ = version_getter("felling")
+except NoPackageError:
+    pass
