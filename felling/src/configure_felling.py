0f804c6da972b5fbbbd856101bd60d09884efb54
Josh <joshuaarkirk@hotmail.co.uk>
2021-03-25 16:38:23 +0000

    remove error handling for permission error, not sure it is required

--- felling/src/configure_felling.py
+++ felling/src/configure_felling.py
@@ -171,14 +171,7 @@ def configure(
         # Ironically print as logs are not yet set up
         # TODO: set up simple config?
         logger.warning(f"Log path does not exist, will create {log_path.absolute()}")
-        try:
-            log_path.mkdir()
-        except PermissionError as e:
-            print(
-                f"Runtime does not have correct permissions to create a log dir, felling cannot configure"
-            )
-            print(e)
-            return
+        log_path.mkdir()
 
     # read in logger config
     with open(
