aaab6ea94f555d280b68e005186ba4b2ef62b8dc
Josh <joshuaarkirk@hotmail.co.uk>
2021-04-06 20:18:39 +0100

    Added a method for checking email addresses

--- felling/src/email.py
+++ felling/src/email.py
@@ -1,11 +1,33 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
-from typing import List, Optional, Union
+from typing import List, Optional, Sequence, Tuple, Union
 import ssl
+import os
+import re
 import logging
 
 logger = logging.getLogger(__name__)
+
+
+def check_email_address(email_to_check: str):
+    """
+    Check an email address is valid
+
+    Parameters
+    ----------
+    email_to_check : str
+        Email address to check
+
+    Raises
+    ------
+    ValueError
+        If the email address is not RFC_5322 compliant
+    """
+    RFC_5322_regex = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+    match = re.match(RFC_5322_regex, email_to_check)
+    if match is None:
+        raise ValueError(f"{email_to_check} is not a valid email address")
 
 
 def send_email(
